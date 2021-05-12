507c9f295542247368bd5206010975ece7436ad2
Aleksandr Chudak <63600579+AlexChudak@users.noreply.github.com>
2021-05-12 21:50:52 +0300

    Commit 1
    Add ContactForm

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import Form from './Form';
+import ContactForm from './components/ContactForm';
+import Container from './components/Container'
+
 
 class App extends Component {
- 
-  formSubmitHandler = data => {
-    console.log(data);
-  }
- 
+
+ state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+ };
+  
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value })
+  };
+
+  addContact = contact => {
+    if (!this.hasContacts(contact.name)) {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+
+    } else {
+      alert(`${contact.name} is already in contacts`);
+    }
+  };
+  
+  
+  
+  hasContacts = name => {
+    return this.state.contacts.find(contact => {
+      return contact.name.toLocaleLowerCase() === name.toLocaleLowerCase();
+    });
+  };
+
 
   render() {
     return (
-      <div>
-      <Form onSubmit={this.formSubmitHandler} />
-
-    
+      <Container>  
+  <h1>Phonebook</h1>
+  <ContactForm onSubmit={this.addContact} />
+<div>
+  <h2>Contacts</h2>
+  {/* <Filter ... /> */}
+  {/* <ContactList ... /> */}
 </div>
-
+</Container> 
     )
   }
 }
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import PropType from 'prop-types';
+import style from './ContactForm.module.css'
+import shortid from 'shortid';
+
+
+class ContactForm extends Component {
+    initialState = {
+        name: '',
+        number: '',
+    };
+    state = {
+        name: '',
+        number:'',
+    }
+
+    nameInputId = shortid.generate();
+    numberInputId = shortid.generate();
+
+    handleChange = event => {
+        const { name, value } = event.currentTarget;
+        this.setState({[name]:value})
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        const contact = {
+            id: shortid.generate(),
+            name: this.state.name,
+            number: this.state.number,
+        }
+
+        this.props.onSubmit(contact);
+
+        this.reset();
+    };
+
+    reset = () => {
+        this.setState(this.initialState);
+    }
+
+    render() {
+        const { name, number } = this.state;
+        return (
+            <form>
+                <label>
+                    Name
+                    <input
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+                        required
+                        value={name}
+                        onChange={this.handleChange}
+                        id={this.nameInputId}/>
+                </label>
+                <label>
+                    <input
+                    type="tel"
+                     name="number"
+                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                     title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                        required
+                        value={number}
+                        onChange={this.handleChange}
+                        id={this.numberInputId}
+                    /> 
+                </label>
+                <button className={style.button} type="submit">
+                    Add contact</button>
+            </form>
+        )
+    }
+}
+
+ContactForm.PropType = {
+    onSubmit:PropType.func.isRequired,
+}
+
+export default ContactForm;
